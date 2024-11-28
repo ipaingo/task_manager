9c13db381784a0128d47ef6c47936080fd2407a5
Sofia <90217362+ipaingo@users.noreply.github.com>
2024-11-28 23:21:12 +0300

    Update test_usermanager.py

--- test_task_manager/test_usermanager.py
+++ test_task_manager/test_usermanager.py
@@ -1,39 +1,27 @@
 import unittest
-from user import User
-from task import Task
+from usermanager import *
 
 class TestUserManager(unittest.TestCase):
     def setUp(self):
         """Подготовка данных перед каждым тестом."""
-        self.user = User("Анна")
-        self.task1 = Task("Задача 1", "Описание 1")
-        self.task2 = Task("Задача 2", "Описание 2")
+        users.clear()  # Очищаем список пользователей перед каждым тестом
+        self.user1 = User("Анна")
+        self.user2 = User("Иван")
+        users.append(self.user1)
+        users.append(self.user2)
 
-    def test_add_task(self):
-        """Тест добавления задачи пользователю."""
-        self.user.add_task(self.task1)
-        self.assertIn(self.task1, self.user.tasks)
+    def test_find_user_existing(self):
+        """Тест поиска существующего пользователя."""
+        user = find_user("Анна")
+        self.assertIsNotNone(user)
+        self.assertEqual(user.name, "Анна")
 
-    def test_add_duplicate_task(self):
-        """Тест добавления дубликата задачи."""
-        self.user.add_task(self.task1)
-        self.user.add_task(self.task1)  # Попытка добавить ту же задачу
-        self.assertEqual(len(self.user.tasks), 1)
+    def test_find_user_non_existing(self):
+        """Тест поиска несуществующего пользователя."""
+        user = find_user("Сергей")
+        self.assertIsNone(user)
 
-    def test_get_info(self):
-        """Тест получения информации о пользователе и его задачах."""
-        self.user.add_task(self.task1)
-        info = self.user.get_info()
-        self.assertIn("Анна", info)
-        self.assertIn("Задача 1", info)
-
-    def test_remove_task(self):
-        """Тест удаления задачи по названию."""
-        self.user.add_task(self.task1)
-        self.user.remove_task("Задача 1")
-        self.assertNotIn(self.task1, self.user.tasks)
-
-    def test_remove_non_existing_task(self):
-        """Тест удаления несуществующей задачи."""
-        result = self.user.remove_task("NonExistingTask")
-        self.assertFalse(result)
+    def test_find_user_case_sensitive(self):
+        """Тест поиска с учётом регистра."""
+        user = find_user("анна")
+        self.assertIsNone(user)
